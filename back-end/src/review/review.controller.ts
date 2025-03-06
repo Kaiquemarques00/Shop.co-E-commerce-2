dc7ca4c164fd814c7cbeaa3de03ef1b62107e942
Kaique marques <kaiquemarques581@outlook.com>
2025-03-06 10:25:30 -0300

    Adicionando feature de review (get and post)

---
+++ back-end/src/review/review.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from "@nestjs/common";
+import { ReviewService } from "./review.service";
+import { CreateReviewDTO } from "./dto/create-review.dto";
+
+
+@Controller("reviews")
+export class ReviewController {
+    
+    constructor(private readonly reviewService: ReviewService) {}
+
+    @Post()
+    async create(@Body() data: CreateReviewDTO) {
+        return this.reviewService.create(data)
+    }
+
+    @Get()
+    async list() {
+        return this.reviewService.list();
+    }
+
+    @Get(":id")
+    async show(@Param('id') id: string) {
+        return this.reviewService.show(id);
+    }
+}
---
+++ back-end/src/review/review.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { CreateReviewDTO } from "./dto/create-review.dto";
+
+@Injectable()
+export class ReviewService {
+
+    constructor(private readonly prisma: PrismaService) {}
+
+    async create(data: CreateReviewDTO) {
+
+        return await this.prisma.review.create({
+            data
+        });
+    }
+
+    async list() {
+        return await this.prisma.review.findMany();
+    }
+
+    async show(id: string) {
+
+        const reviews = await this.prisma.review.findUnique({
+            where: {
+                id
+            }
+        });
+
+        if (!reviews) return new NotFoundException(`A review ${id} não existe.`);
+
+        return reviews;
+    }
+
+    async exists(id: string) {
+        if (!(await this.show(id))) {
+            throw new NotFoundException(`A review ${id} não existe.`);
+        }
+    }
+}
