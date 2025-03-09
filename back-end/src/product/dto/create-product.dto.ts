bf33e4e660eddb4dc2b056093f770050510eb1fb
Kaique marques <kaiquemarques581@outlook.com>
2025-03-08 21:45:56 -0300

    Arrumando exclusao do DTO

---
+++ back-end/src/product/dto/create-product.dto.ts
@@ -0,0 +1,46 @@
+import { IsNumber, IsString, IsUUID, Max, Min, IsOptional } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class CreateProductDTO {
+    @IsString()
+    name_product: string;
+
+    @IsString()
+    description: string;
+
+    @IsString()
+    price: string;
+
+    @IsString()
+    color: string[];
+
+    @IsString()
+    size: string[];
+
+    @IsOptional()
+    @IsString()
+    tag?: string;
+
+    @IsOptional()
+    @IsString()
+    price_discount?: string;
+
+    @IsUUID()
+    category_id: string;
+
+    @IsNumber()
+    @Transform(({ value }) => Number(value))
+    amount: number;
+
+    @IsNumber()
+    @Min(0)
+    @Max(5)
+    @Transform(({ value }) => Number(value))
+    rating: number;
+
+    @IsOptional()
+    @IsNumber()
+    discount?: number;
+
+    product_imgs?: string[]; // Agora aceita múltiplas imagens
+}
