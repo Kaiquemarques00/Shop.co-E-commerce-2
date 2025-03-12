5bce53e9448593a719397e87bfeea6f5d1f7a399
Kaique marques <kaiquemarques581@outlook.com>
2025-03-12 16:03:55 -0300

    Arrumando erro do eslint 2

--- back-end/src/product/product.service.ts
+++ back-end/src/product/product.service.ts
@@ -15,7 +15,7 @@ export class ProductService {
   ) {}
 
   async create(data: CreateProductDTO, file: Express.Multer.File) {
-    const upload = await this.cloudinary.uploadImage(file);
+    //const upload = await this.cloudinary.uploadImage(file);
     //const imageUrl = upload.url;
 
     const product = await this.prisma.product.create({
