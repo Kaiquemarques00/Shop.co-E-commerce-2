f8ab13780dd8508c140f2549dcc21fe5c5ffb835
Kaique marques <kaiquemarques581@outlook.com>
2025-03-12 16:26:11 -0300

    Arrumando erro de deploy api

--- back-end/src/product/product.service.ts
+++ back-end/src/product/product.service.ts
@@ -24,7 +24,7 @@ export class ProductService {
         amount: Number(data.amount),
         rating: Number(data.rating),
         discount: data.discount ? Number(data.discount) : null,
-        url_image: imageUrl
+        url_image: String(imageUrl)
       },
     });
 
